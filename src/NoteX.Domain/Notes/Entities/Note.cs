3dab6811d7c8537dfa064225a859c450e5a1928b
Miguel0101 <miguelmagnofn@gmail.com>
2025-11-26 20:49:38 -0300

    feat(docs): Note domain model documented

--- src/NoteX.Domain/Notes/Entities/Note.cs
+++ src/NoteX.Domain/Notes/Entities/Note.cs
@@ -3,6 +3,10 @@ using NoteX.Domain.Users.Entities;
 
 namespace NoteX.Domain.Notes.Entities;
 
+/// <summary>
+/// Note - Domain Model
+/// <para>Represents a note with title and content.</para>
+/// </summary>
 public class Note
 {
     public Guid Id { get; private set; }
@@ -24,6 +28,13 @@ public class Note
         UpdatedAt = null;
     }
 
+    /// <summary>
+    /// Creates a new note with the user ID, title and content.
+    /// </summary>
+    /// <param name="userId">The owner of this note</param>
+    /// <param name="title">The title</param>
+    /// <param name="content">The content</param>
+    /// <returns>The created note.</returns>
     public static Note Create(Guid userId, Title title, Content content)
     {
         Note note = new(userId, title, content);
@@ -31,6 +42,11 @@ public class Note
         return note;
     }
 
+    /// <summary>
+    /// Updates the title of a existing note.
+    /// </summary>
+    /// <param name="title">The updated title</param>
+    /// <returns>The updated note.</returns>
     public Note UpdateTitle(Title title)
     {
         if (title == Title)
@@ -42,6 +58,11 @@ public class Note
         return this;
     }
 
+    /// <summary>
+    /// Updates the content of a existing note.
+    /// </summary>
+    /// <param name="content">The updated content</param>
+    /// <returns>The updated note.</returns>
     public Note UpdateContent(Content content)
     {
         if (content == Content)
